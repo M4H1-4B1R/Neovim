4e95cb02525f733a6da6290f4052dbd34e804a3f
autonomuscoder <mahiabirgit@tutanota.com>
2023-10-20 22:31:12 +0600

    catppuccin and finecmdline

--- nvim/lua/core/configs/catppuccin.lua
+++ nvim/lua/core/configs/catppuccin.lua
@@ -1,8 +1,10 @@
 require("catppuccin").setup({
-    flavour = "mocha", -- latte, frappe, macchiato, mocha
-    background = { -- :h background
-        light = "latte",
-        dark = "mocha",
-    },
-    transparent_background = true,
-	})
+	flavour = "mocha",   -- latte, frappe, macchiato, mocha
+	background = {       -- :h background
+		light = "latte",
+		dark = "mocha",
+	},
+	transparent_background = true,
+})
+
+vim.cmd.colorscheme "catppuccin"
--- nvim/lua/core/keys.lua
+++ nvim/lua/core/keys.lua
@@ -6,7 +6,7 @@ vim.keymap.set("n", "ss", ':split<Return><C-w>w', { silent = true })
 vim.keymap.set("n", "sv", ':vsplit<Return><C-w>w', { silent = true })
 
 --tab
-vim.keymap.set("n", "nt", ":tabedit<Return>", {silent = true})
+vim.keymap.set("n", "nt", ":tabedit<Return>", { silent = true })
 
 -- Window Movement
 vim.keymap.set('n', '<leader>r', '<C-w>w') --rotate between open windows
@@ -14,3 +14,6 @@ vim.keymap.set('', 'sh', '<C-w>h')
 vim.keymap.set('', 'sj', '<C-w>j')
 vim.keymap.set('', 'sk', '<C-w>k')
 vim.keymap.set('', 'sl', '<C-w>l')
+
+-- Fine cmdline
+vim.api.nvim_set_keymap('n', ':', '<cmd>FineCmdline<CR>', { noremap = true })
