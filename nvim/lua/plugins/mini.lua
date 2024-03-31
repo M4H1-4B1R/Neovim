435d312b68a2f06f6c157eeb39ad5110585052d2
autonomuscoder <mahiabirgit@tutanota.com>
2024-03-31 17:03:34 +0600

    added mini colorizer for hex colors

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,10 +1,15 @@
 return {
-	'echasnovski/mini.nvim', 
-	version = '*',
+	"echasnovski/mini.nvim",
+	version = "*",
 	config = function()
-		require('mini.statusline').setup()
-		require('mini.indentscope').setup()
-		require('mini.hipatterns').setup()
-		require('mini.pairs').setup()
-	end
+		local hipatterns = require("mini.hipatterns")
+		require("mini.statusline").setup()
+		require("mini.indentscope").setup()
+		require("mini.hipatterns").setup({
+			highlighters = {
+				hex_color = hipatterns.gen_highlighter.hex_color(),
+			},
+		})
+		require("mini.pairs").setup()
+	end,
 }
