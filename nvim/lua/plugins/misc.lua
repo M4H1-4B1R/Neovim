7f29ed4e1233d345cd60ec78668bdac593de4b64
autonomuscoder <mahiabirgit@tutanota.com>
2024-04-04 19:58:35 +0600

    added undotree plugin

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -16,4 +16,12 @@ return {
 			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
 		},
 	},
+	{
+		"jiaoshijie/undotree",
+		dependencies = "nvim-lua/plenary.nvim",
+		config = true,
+		keys = { -- load the plugin only when using it's keybinding:
+			{ "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+		},
+	},
 }
