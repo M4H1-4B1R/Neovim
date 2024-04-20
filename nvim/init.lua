0881d8e59cb9708a48e30ace95780ec371b8fb08
autonomuscoder <mahiabirgit@tutanota.com>
2024-04-20 16:02:19 +0600

    live-server.nvim plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -50,6 +50,11 @@ vim.o.autoindent = true
 vim.o.smartindent = true
 vim.o.shell = "fish"
 
+-- netrw customization
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+
 -- disable some default providers
 vim.g["loaded_node_provider"] = 0
 vim.g["loaded_python3_provider"] = 0
--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -4,7 +4,7 @@ return {
 		lazy = false,
 		priority = 1000,
 		config = function()
-			--			vim.cmd.colorscheme("midnight")
+			--	vim.cmd.colorscheme("midnight")
 		end,
 	},
 	{
@@ -12,17 +12,15 @@ return {
 		lazy = false,
 		priority = 1000,
 		config = function()
-			-- Lua
 			require("onedark").setup({
-				-- Main options --
-				style = "darker", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-				transparent = true, -- Show/hide background
-				term_colors = false, -- Change terminal color as per the selected theme style
-				ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+				style = "deep",           -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+				transparent = true,       -- Show/hide background
+				term_colors = false,      -- Change terminal color as per the selected theme style
+				ending_tildes = false,    -- Show the end-of-buffer tildes. By default they are hidden
 				cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
 
 				-- toggle theme style ---
-				toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+				toggle_style_key = nil,                                                          -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
 				toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" }, -- List of styles to toggle between
 
 				-- Change code style ---
@@ -52,7 +50,65 @@ return {
 					background = false, -- use background color for virtual text
 				},
 			})
-			require("onedark").load()
+			--		require("onedark").load()
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "auto", -- latte, frappe, macchiato, mocha
+				background = { -- :h background
+					light = "latte",
+					dark = "mocha",
+				},
+				transparent_background = true, -- disables setting the background color.
+				show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+				term_colors = false,        -- sets terminal colors (e.g. `g:terminal_color_0`)
+				dim_inactive = {
+					enabled = false,          -- dims the background color of inactive window
+					shade = "dark",
+					percentage = 0.15,        -- percentage of the shade to apply to the inactive window
+				},
+				no_italic = false,          -- Force no italic
+				no_bold = false,            -- Force no bold
+				no_underline = false,       -- Force no underline
+				styles = {                  -- Handles the styles of general hi groups (see `:h highlight-args`):
+					comments = { "italic" },  -- Change the style of comments
+					conditionals = { "italic" },
+					loops = {},
+					functions = {},
+					keywords = {},
+					strings = {},
+					variables = {},
+					numbers = {},
+					booleans = {},
+					properties = {},
+					types = {},
+					operators = {},
+					-- miscs = {}, -- Uncomment to turn off hard-coded styles
+				},
+				color_overrides = {},
+				custom_highlights = {},
+				default_integrations = true,
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					nvimtree = true,
+					treesitter = true,
+					notify = false,
+					mini = {
+						enabled = true,
+						indentscope_color = "",
+					},
+					-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+				},
+			})
+
+			-- setup must be called before loading
+			vim.cmd.colorscheme("catppuccin")
 		end,
 	},
 }
