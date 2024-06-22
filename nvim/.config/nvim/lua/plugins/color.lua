e2f480ea351b9cdeb78bbd71db62e9fe792e31ef
M4H1-4B1R <mahiabirgit@tutanota.com>
2024-06-22 12:48:40 +0600

    gruvbox for the win

--- nvim/.config/nvim/lua/plugins/color.lua
+++ nvim/.config/nvim/lua/plugins/color.lua
@@ -7,19 +7,34 @@ function HateSolidBackground(color)
 end
 
 return {
-	'f4z3r/gruvbox-material.nvim',
-	name = 'gruvbox-material',
-	lazy = false,
+	"ellisonleao/gruvbox.nvim",
 	priority = 1000,
 	config = function()
-		require('gruvbox-material').setup({
-			background = {
-				transparent = true,
-			}
+		-- Default options:
+		require("gruvbox").setup({
+			terminal_colors = true, -- add neovim terminal colors
+			undercurl = true,
+			underline = true,
+			bold = true,
+			italic = {
+				strings = true,
+				emphasis = true,
+				comments = true,
+				operators = false,
+				folds = true,
+			},
+			strikethrough = true,
+			invert_selection = false,
+			invert_signs = false,
+			invert_tabline = false,
+			invert_intend_guides = false,
+			inverse = true, -- invert background for search, diffs, statuslines and errors
+			contrast = "", -- can be "hard", "soft" or empty string
+			palette_overrides = {},
+			overrides = {},
+			dim_inactive = false,
+			transparent_mode = true,
 		})
-	vim.cmd.colorscheme "gruvbox-material"
-	HateSolidBackground()
-	end
+		vim.cmd("colorscheme gruvbox")
+	end,
 }
-
-
