10d6286ae54af956de6aa2466c23d6a3954cffdc
M4H1-4B1R <mahiabirgit@tutanota.com>
2024-09-03 21:33:26 +0600

    nice icons for lazy ui

--- nvim/.config/nvim/lua/custom/lazy.lua
+++ nvim/.config/nvim/lua/custom/lazy.lua
@@ -4,19 +4,29 @@ vim.g.maplocalleader = ' '
 -- bootstraping lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins", {
 	checker = {
 		enabled = true,
 		notify = false,
+	},
+	ui = {
+		icons = {
+			cmd = '⌘',
+			ft = "",
+			lazy = "󰂠 ",
+			loaded = "",
+			not_loaded = "",
+			init = '⚙',
+		},
 	}
 })
