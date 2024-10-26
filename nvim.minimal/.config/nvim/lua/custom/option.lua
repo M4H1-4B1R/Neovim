145a4a7ea9a1aa3032c444db797863274f302dee
M4H1-4B1R <mahiabirgit@tutanota.com>
2024-10-26 22:23:21 +0600

    formatting some files properly in neovim

--- nvim.minimal/.config/nvim/lua/custom/option.lua
+++ nvim.minimal/.config/nvim/lua/custom/option.lua
@@ -11,7 +11,7 @@ vim.o.hlsearch = false
 vim.wo.relativenumber = true
 
 -- Enable mouse mode
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 
 -- syntaxt highlight
 vim.g.syntax = true
@@ -25,7 +25,7 @@ vim.o.cmdheight = 0
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard = "unnamedplus"
 
 -- Enable break indent
 vim.o.breakindent = true
@@ -41,7 +41,7 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 
 -- Keep signcolumn off by default
-vim.wo.signcolumn = 'no'
+vim.wo.signcolumn = "no"
 
 -- Decrease update time
 vim.o.updatetime = 250
@@ -55,7 +55,7 @@ vim.o.showmode = false
 -- vim.o.inccommand = 'split'
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
+vim.o.completeopt = "menuone,noselect"
 
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
@@ -63,12 +63,16 @@ vim.o.shiftwidth = 2
 vim.o.tabstop = 2
 vim.o.autoindent = true
 vim.o.smartindent = true
-vim.o.shell = "fish"
+vim.o.shell = "bash"
+-- ignore node modules
+vim.opt.wildignore:append({ "/*node_modules*/" })
+vim.opt.path:append({ "**" })
 
 -- netrw customization
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+-- disable netrw
 -- vim.g.loaded_netrwPlugin = 1
 -- vim.g.loaded_netrw = 1
 
@@ -82,21 +86,20 @@ vim.g["loaded_ruby_provider"] = 0
 if vim.g.neovide then
 	vim.o.guifont = "MesloLGS Nerd Font:h13"
 	vim.g.neovide_transparency = 1
-	vim.g.neovide_theme = 'auto'
+	vim.g.neovide_theme = "auto"
 	vim.g.neovide_refresh_rate = 60
 end
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-	desc = 'Highlight when yanking (copying) text',
-	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
 	callback = function()
 		vim.highlight.on_yank()
 	end,
 })
-
 
 -- from the primeagen
 -- function HateSolidBackground(color)
