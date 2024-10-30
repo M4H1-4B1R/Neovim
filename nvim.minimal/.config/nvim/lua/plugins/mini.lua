072d85837bf7aa459f77bc0d681232097e09d6fb
M4H1-4B1R <mahiabirgit@tutanota.com>
2024-10-30 20:35:41 +0600

    trying statusbarless nvim using mini tabs

--- nvim.minimal/.config/nvim/lua/plugins/mini.lua
+++ nvim.minimal/.config/nvim/lua/plugins/mini.lua
@@ -12,6 +12,24 @@ return {
 		})
 		require("mini.pairs").setup()
 		require("mini.notify").setup()
-		require("mini.statusline").setup()
+		require("mini.tabline").setup()
+		-- require("mini.statusline").setup()
+		require("mini.files").setup()
+		require("mini.icons").setup({
+			opts = {
+				file = {
+					[".eslintrc.js"] = { glyph = "󰱺", hl = "MiniIconsYellow" },
+					[".node-version"] = { glyph = "", hl = "MiniIconsGreen" },
+					[".prettierrc"] = { glyph = "", hl = "MiniIconsPurple" },
+					[".yarnrc.yml"] = { glyph = "", hl = "MiniIconsBlue" },
+					["eslint.config.js"] = { glyph = "󰱺", hl = "MiniIconsYellow" },
+					["package.json"] = { glyph = "", hl = "MiniIconsGreen" },
+					["tsconfig.json"] = { glyph = "", hl = "MiniIconsAzure" },
+					["tsconfig.build.json"] = { glyph = "", hl = "MiniIconsAzure" },
+					["yarn.lock"] = { glyph = "", hl = "MiniIconsBlue" },
+				},
+			},
+		})
 	end,
+	vim.keymap.set("n", "<leader>fe", ":lua MiniFiles.open()<CR>", { noremap = true, silent = true }),
 }
