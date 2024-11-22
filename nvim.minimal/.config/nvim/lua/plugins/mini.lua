05711afd6b3ead3f68af4cd410a835574d4f80ce
M4H1-4B1R <mahiabirgit@tutanota.com>
2024-11-22 19:26:24 +0600

    added mini.git and switched to mini base16 colorscheme

--- nvim.minimal/.config/nvim/lua/plugins/mini.lua
+++ nvim.minimal/.config/nvim/lua/plugins/mini.lua
@@ -12,9 +12,11 @@ return {
 		})
 		require("mini.pairs").setup()
 		require("mini.notify").setup()
-		require("mini.tabline").setup()
-		-- require("mini.statusline").setup()
-		require("mini.files").setup()
+		-- require("mini.tabline").setup()
+		require("mini.git").setup()
+		require("mini.diff").setup()
+		require("mini.statusline").setup()
+		-- require("mini.files").setup()
 		require("mini.icons").setup({
 			opts = {
 				file = {
@@ -30,6 +32,36 @@ return {
 				},
 			},
 		})
+		require("mini.base16").setup({
+			palette = {
+				base00 = "#040507",
+				base01 = "#3c3836",
+				base02 = "#504945",
+				base03 = "#665c54",
+				base04 = "#bdae93",
+				base05 = "#d5c4a1",
+				base06 = "#ebdbb2",
+				base07 = "#fbf1c7",
+				base08 = "#fb4934", --red
+				base09 = "#fe8019", -- orange
+				base0A = "#fabd2f", -- yellow
+				base0B = "#b8bb26", -- green
+				base0C = "#8ec07c", -- cyan
+				base0D = "#83a598", -- blue
+				base0E = "#d3869b", -- purple
+				base0F = "#d65d0e", -- brown
+			},
+			use_cterm = true,
+			plugins = {
+				default = false,
+				["echasnovski/mini.nvim"] = true,
+			},
+		})
+
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "Normal", { ctermbg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { ctermbg = "none" })
 	end,
-	vim.keymap.set("n", "<leader>fe", ":lua MiniFiles.open()<CR>", { noremap = true, silent = true }),
+	-- vim.keymap.set("n", "<leader>fe", ":lua MiniFiles.open()<CR>", { noremap = true, silent = true }),
 }
