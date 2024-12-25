e05fbd5314e2d0205bc112810890c2a2184f0b69
M4H1-4B1R <mahiabirgit@tutanota.com>
2024-12-25 12:00:13 +0600

    changed shell to fish

--- nvim.minimal/.config/nvim/lua/custom/option.lua
+++ nvim.minimal/.config/nvim/lua/custom/option.lua
@@ -63,7 +63,7 @@ vim.o.shiftwidth = 2
 vim.o.tabstop = 2
 vim.o.autoindent = true
 vim.o.smartindent = true
-vim.o.shell = "bash"
+vim.o.shell = "fish"
 -- ignore node modules
 vim.opt.wildignore:append({ "/*node_modules*/" })
 vim.opt.path:append({ "**" })
