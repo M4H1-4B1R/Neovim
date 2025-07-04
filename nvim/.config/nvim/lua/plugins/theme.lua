5bbc5fdd38354f708149ce4fc6fccbeaca004192
M4H1-4B1R <mahiabirgit@tutanota.com>
2025-07-04 15:01:20 +0600

    added catppuccin transparency

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,12 +1,23 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {
-    transparent = true,
-    styles = {
-      floats = "transparent",
-      sidebars = "transparent",
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true,
+      styles = {
+        floats = "transparent",
+        sidebars = "transparent",
+      },
+    },
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha",
+      transparent_background = true,
     },
   },
 }
