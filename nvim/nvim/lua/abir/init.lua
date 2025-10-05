02d8bffd31c564932475b04a07494ee9e8a79fa1
M4H1-4B1R <mahiabirgit@tutanota.com>
2025-10-05 18:37:25 +0600

    changing neovim config

---
+++ nvim/nvim/lua/abir/init.lua
@@ -0,0 +1,3 @@
+require("abir.options")
+require("abir.lazy")
+require("abir.keybinds")
---
+++ nvim/nvim/lua/plugins/mini.lua
@@ -0,0 +1,17 @@
+return {
+	"echasnovski/mini.nvim",
+	version = "*",
+	event = "BufEnter",
+	config = function()
+		local hipatterns = require("mini.hipatterns")
+		require("mini.indentscope").setup()
+		hipatterns.setup({
+			highlighters = {
+				hex_color = hipatterns.gen_highlighter.hex_color(),
+			},
+		})
+		require("mini.pairs").setup()
+		require("mini.notify").setup()
+		require("mini.statusline").setup()
+	end,
+}
